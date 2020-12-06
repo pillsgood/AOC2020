542a4e4b4e68fa088aad000539bb5ad21cd8315a
Pillsgood <arashm35@gmail.com>
2020-12-06 14:43:14 +0800

    add Day 6;

---
+++ AOC2020/Day6.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
+using Pillsgood.AdventOfCode.Abstractions;
+using Pillsgood.AdventOfCode.Core;
+
+namespace AOC2020
+{
+    public partial class Day6 : IPuzzle
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IPuzzleInput<IEnumerable<char[][]>>>(provider =>
+                new PuzzleInput<IEnumerable<char[][]>>(provider, Process));
+        }
+
+        private static IEnumerable<char[][]> Process(string value)
+        {
+            return value.Split("\n\n", StringSplitOptions.RemoveEmptyEntries).Select(s => s.Trim())
+                .Select(group => group.Split('\n').Select(person => person.ToArray()).ToArray());
+        }
+
+        [Part(1)]
+        private string Part1(IPuzzleInput<IEnumerable<char[][]>> input)
+        {
+            var groupDistinctQuestions = input.Value
+                .Select(group => group.SelectMany(chars => chars.Select(c => c)).Distinct().ToArray());
+            var answer = groupDistinctQuestions.Aggregate(0, (i, chars) => i + chars.Length);
+            return answer.ToString();
+        }
+
+        [Part(2)]
+        private string Part2(IPuzzleInput<IEnumerable<char[][]>> input)
+        {
+            var groupAllQuestions = input.Value
+                .Select(group => group.SelectMany(person => person.Select(c => c)).Distinct()
+                    .Count(c => group.All(person => person.Contains(c))));
+            var answer = groupAllQuestions.Aggregate((i, i1) => i + i1);
+            return answer.ToString();
+        }
+    }
+}
