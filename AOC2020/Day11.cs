0ddd077d21e6377ae3a8c01e4ccb58504d2059e0
Pillsgood <arashm35@gmail.com>
2020-12-14 20:44:32 +0800

    remove unused directives;

--- AOC2020/Day11.cs
+++ AOC2020/Day11.cs
@@ -1,9 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
 using System.Linq;
-using System.Numerics;
 using AOC2020.DataTypes;
 using Microsoft.Extensions.DependencyInjection;
 using Pillsgood.AdventOfCode.Abstractions;
